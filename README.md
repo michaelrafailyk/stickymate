1f8d3fabea158cf0413173407fb1c70233ea6f88
Michael Rafaylik <michael@Mac-mini-Michael.local>
2019-11-14 20:32:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,150 @@
+StickyMate is a tool that designed to help web developers easily create animations without JavaScript knowledge, just setting animation params in JSON format directly in the HTML markup. StickyMate combines extended sticky positioning and scroll-based animation features, so it can catch an element on the screen and animate it while the user scrolls the page. Use your imagination to implement great ideas that will present your content in the most spectacular way.
+
+## Demo
+
+[StickyMate demonstration](https://rafaylik.github.io/stickymate/)
+
+## Installing
+
+- [Download](https://github.com/rafaylik/stickymate/archive/master.zip) and unzip, or just `$ git clone https://github.com/rafaylik/stickymate.git`
+- Place a `stickymate.js` in the directory of your application.
+- Include it at the `head` section or at the end of the `body` tag.
+``` html
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <script src="stickymate.js" async defer></script>
+
+    </head>
+    <body>
+
+        // elements
+
+    </body>
+</html>
+```
+
+## Usage
+
+- Add a `data-sticky` or `data-animation` attributes (framed in single quotes) to your element together or apart.
+``` html
+<div data-sticky='{...}' data-animation='{...}'></div>
+```
+- Set parameters (with double quotes inside) for `data-sticky` and `data-animation` attributes by the following scheme (for simplicity, look at this in a hierarchical view).
+``` html
+<div
+    data-sticky='{
+        "start": "position",        // position from the top of the viewport when the element sticks to the screen
+        "end": "position"           // position when the element will no longer sticky, extends the min-height of the parent container
+    }'
+    data-animation='{
+        "CSS property": {
+            "position": "value",    // animate from
+            "position": "value",    // optional, intermediate keys (.. to / from ..)
+            "position": "value"     // animate to
+        },
+        "CSS property": {...}       // optional, additional CSS properties
+    }'
+></div>
+```
+- The `data-sticky` attribute can contain only two params, namely `start` and `end`.
+- If there is no need to extends the min-height of the parent container, you can specify `0` for the `end` value.
+- The `data-animation` attribute can contain one or more animated CSS properties, separated by commas.
+- Every `data-animation` CSS property can contain two or more position keys, separated by commas.
+- Use JSON format with double quotes inside for every key and value.
+- Set the name of animated CSS property without any vendor prefixes (`-webkit-` is added automatically).
+- Inside one animated CSS property, for each value, keep the same order of sub values, if there are several. It is necessary for the correct operation of the autocomplete function.
+``` html
+<div data-animation='{"transform": {
+    "-75vh": "scale(1) translate(0, 0)",       // sub values in order - scale, translate
+    "-50vh": "scale(.5) translate(0, 50%)",    // correctly, the same sub values order
+    "-25vh": "translate(0, 0) scale(1)"        // wrong, sub values order is different
+}}'></div>
+```
+- Inside one animated CSS property, use the same units for setting position keys.
+- Units of position keys should be `px` (pixels), `vh` (viewport height) or `vw` (viewport width).
+- Units of `data-animation` CSS property values are set the same as in CSS.
+- `0` position can be set without units.
+- `-100vh` = top of element reached the bottom of the viewport.
+- `0vh` or `0px` or just `0` = top of element reached the top of the viewport.
+- `100vh` = top of element reached the top of the viewport + one screen height has been scrolled after this.
+
+## Animated CSS properties
+
+- You can animate any CSS property whose value contains just numbers or numbers in `%`, `px`, `vw`, `vh`, `em`, `rem`, `deg`.
+- You cannot animate a `color` CSS property and a properties whose value contains: `left`, `right`, `top`, `bottom`, `center`, `none`, `auto`, `transparent` etc.
+- Validated CSS properties:
+    - `transform: matrix() scale() translate() rotate() translate3D() rotate3D()`
+    - `opacity: 0-1`
+
+## Examples
+
+- Sticky element.
+``` html
+<div data-sticky='{"start": "0", "end": "200vh"}'></div>
+```
+When top of the element reaches the top of the viewport (`"start": "0"`), it will stick on the screen until you scroll up two viewport height (`"end": "200vh"`) from that moment.
+
+- Animated element with one CSS property and two position keys.
+``` html
+<div data-animation='{"opacity": {"-100vh": "0", "-50vh": "1"}}'></div>
+```
+While top of the element traverse from bottom of the viewport `-100vh` to the middle of the viewport `-50vh`, its `opacity` changes from `0` to `1`. Same as `opacity: 0;` and `opacity: 1;` in the CSS. Visually, this is a fade-in effect.
+
+- Animation with one CSS property and multiple position keys.
+``` html
+<div data-animation='{"opacity": {"-100vh": "0", "-75vh": "1", "-25vh": "1", "0vh": ".25"}}'></div>
+```
+In this example, the opacity of an element changes in two stages, from `0` to `1` (the top of the element moves along the lower quarter of the viewport: `-100vh`, `-75vh`) and then from `1` to `0.25` (the top of the element moves along the upper quarter of the viewport: `-25vh`, `0vh`).
+
+- Animation with multiple CSS properties.
+``` html
+<div data-animation='{"opacity": {"-100vh": "0", "-75vh": "1"}, "transform": {"-100vh": "scale(.75)", "-50vh": "scale(1)"}}'></div>
+```
+Each CSS property has its own positioning keys and values.
+
+- Sticky and animated element (two attributes at once).
+``` html
+<div data-sticky='{"start": "0", "end": "100vh"}' data-animation='{"opacity": {"0": "1", "100vh": "0"}}'></div>
+```
+In this case, you can manipulate an element while it is stuck on the screen.
+
+## Browser compatibility
+
+- For `data-sticky`
+    - Chrome: 56
+    - Edge: 16
+    - Firefox: 48
+    - Internet Explorer: No
+    - Opera: 43
+    - Safari: 8
+    - Chrome on Android: 56
+    - Firefox on Android: 59
+    - Safari on iOS: 8
+
+- For `data-animation`
+    - Chrome: 24
+    - Edge: 16
+    - Firefox: 26
+    - Internet Explorer: 11
+    - Opera: 15
+    - Safari: 7
+    - Chrome on Android: Yes
+    - Firefox on Android: 26
+    - Safari on iOS: 7
+
+## Known minor issues
+
+- Sticky element and parents overflow property. Any parent of `data-sticky` element should not have an `overflow` property. Read more about the issue: [Support position:sticky inside an overflow:hidden|auto on general parents](https://github.com/w3c/csswg-drafts/issues/865). If your animation overflows an element (any transformation that enlarges an element and creates a horizontal scroll bar) and you need to use `overflow: hidden`, just follow this scheme:
+    - Doesn't work: `element with overflow: hidden` \> `sticky & animated element`.
+    - Work: `sticky element with overflow: hidden` \> `animated element`.
+- Matrix and rotation. Don't try to rotate the element with the `transform: matrix()` because script doesn't calculate `sin` and `cos` for correct transformation. To rotate, use `transform: rotate()`.
+- Animation and background fill in Safari macOS. If the sticky element doesn't have a `background` fill (`none` or `transparent`), the animation on the child element will not be smooth. **Under review**.
+- Scrolling in Edge (EdgeHTML only). Periodic bugs in positioning animated elements inside parent with `overflow: hidden` when scrolling in the opposite direction. **Under review**.
+
+## Author and license
+
+- **Michael Rafaylik** - [rafaylik](https://github.com/rafaylik) on GitHub.
+
+Project is licensed under the MIT License - see the [LICENSE.md](./LICENSE) file for details.
---
+++ stickymate.js
@@ -0,0 +1,371 @@
+/*
+
+	StickyMate v1.0.2
+	Licensed under the MIT License
+	Copyright 2019 Michael Rafaylik
+	rafaylik@icloud.com
+	https://github.com/rafaylik/stickymate
+
+*/
+
+(function() {
+
+	// sticky section
+	let sticky = {
+		attribute: 'data-sticky',
+		elements: false,
+		container: 'sticky-container',
+		property: 'position: -webkit-sticky; position: sticky;',
+		// check if browser support position: sticky
+		checkSupport: function() {
+			let element = document.createElement('div');
+			element.style.cssText = sticky.property;
+			return element.style.position.match('sticky') ? true : false;
+		},
+		wrap: function() {
+			if (sticky.checkSupport()) {
+				if (!sticky.elements) {
+					sticky.elements = document.querySelectorAll('[' + sticky.attribute + ']');
+					for (let i = 0; i < sticky.elements.length; i++) {
+						// make sure that we wrap sticky only once
+						if (!sticky.elements[i].parentNode.classList.contains(sticky.container)) {
+							// wrap sticky only if its doesn't have a parent
+							if (sticky.elements[i].parentNode.tagName.toLowerCase() == 'body') {
+								let container = document.createElement('div');
+								container.classList.add(sticky.container);
+								container.style.position = 'relative';
+								sticky.elements[i].parentNode.insertBefore(container, sticky.elements[i]);
+								container.appendChild(sticky.elements[i]);
+							} else {
+								sticky.elements[i].parentNode.classList.add(sticky.container);
+								// change static position to relative to properly get the offsetTop in animation.get
+								if (!sticky.elements[i].parentNode.style.position.length) {
+									sticky.elements[i].parentNode.style.position = 'relative';
+								}
+							}
+						}
+					}
+				}
+			}
+		},
+		get: function() {
+			if (sticky.checkSupport()) {
+				element:
+				for (let i = 0; i < sticky.elements.length; i++) {
+					// get params about start/end of sticky position
+					let params = sticky.elements[i].getAttribute(sticky.attribute);
+					try {
+						params = JSON.parse(params);
+					} catch {continue element}
+					if (!params.start || !params.end) continue element;
+					params.verify = [];
+					// break the params string to separate the numbers inside
+					params.verify.push(params.start.split(/(-?\d*\.?\d+)/).filter(function(e) {return e === 0 || e}));
+					params.verify.push(params.end.split(/(-?\d*\.?\d+)/).filter(function(e) {return e === 0 || e}));
+					if (!params.verify[0][0].match(/\d/) || !params.verify[1][0].match(/\d/)) continue element;
+					// verify params for missing values like units type or parentheses etc
+					validation.autocomplete(params.verify);
+					// convert any type of keys to the pixels
+					let start = convert.unitsToPixels(params.verify[0][0], params.verify[0][1]);
+					start = -start + 0;
+					let end = convert.unitsToPixels(params.verify[1][0], params.verify[1][1]);
+					// compose the min-height for the parent container
+					// but get correct offset until the element is not a sticky
+					let originalPosition = sticky.elements[i].style.position;
+					sticky.elements[i].style.position = 'static';
+					end += sticky.elements[i].offsetTop;
+					end += sticky.elements[i].offsetHeight;
+					sticky.elements[i].style.position = originalPosition;
+					// apply params back to an elements
+					sticky.set(sticky.elements[i], sticky.elements[i].parentElement, start, end);
+				}
+			}
+		},
+		set: function(element, container, start, end) {
+			if (!element.style.position.match('sticky')) {
+				// actually make the element sticky
+				element.style.cssText = (element.getAttribute('style') || '') + sticky.property;
+			}
+			element.style.top = start + 'px';
+			container.style.minHeight = end + 'px';
+		}
+	};
+
+	// animation section
+	let animation = {
+		attribute: 'data-animation',
+		elements: false,
+		list: [],
+		status: {
+			before: 'before',
+			active: 'active',
+			after: 'after'
+		},
+		get: function() {
+			if (!animation.elements) {
+				animation.elements = document.querySelectorAll('[' + animation.attribute + ']');
+			}
+			animation.list = [];
+			element:
+			for (let i = 0; i < animation.elements.length; i++) {
+				// get correct top position
+				// if animated element is inside the sticky, its top position moves along scroll
+				let top = correctTop(animation.elements[i]);
+				// get params about position keys and animated values
+				let params = animation.elements[i].getAttribute(animation.attribute);
+				// to preserve the order of the keys, wrap each of them in an array of one element
+				let beforeFirstKey = /\:(\s+)?\{(\s+)?\"/g;
+				let beforeNextKeys = /\"(\s+)?\,(\s+)?\"/g;
+				let afterEachKey = /\"(\s+)?\:(\s+)?\"/g;
+				params = params.replace(beforeFirstKey, ': {"[').replace(beforeNextKeys, '", "[').replace(afterEachKey, ']": "');
+				try {
+					params = JSON.parse(params);
+				} catch {continue element}
+				// create an array and fill it with verified params
+				let paramsVerified = [];
+				for (let property in params) {
+					if (typeof animation.elements[i].style[property] === 'undefined') continue element;
+					if (Object.keys(params[property]).length < 2) continue element;
+					let list = {};
+					list[property] = {
+						'position': [],
+						'values': [],
+						'status': null,
+						'locked': false
+					};
+					// break the params string to separate the numbers inside
+					keys:
+					for (let key in params[property]) {
+						let position = key.replace(/\[|\]/g, '').split(/(-?\d*\.?\d+)/).filter(function(e) {return e === 0 || e});
+						if (!position[0].match(/\d/)) continue keys;
+						for (let k = 0; k < list[property]['position'].length; k++) {
+							if (position[0] === list[property]['position'][k][0]) continue keys;
+						}
+						let values = params[property][key].split(/(-?\d*\.?\d+)/).filter(function(e) {return e === 0 || e});
+						list[property]['position'].push(position);
+						list[property]['values'].push(values);
+					}
+					// verify params for missing values like units type or parentheses etc
+					validation.autocomplete(list[property]['position']);
+					validation.autocomplete(list[property]['values']);
+					for (let j = 0; j < list[property]['position'].length; j++) {
+						let numbers = list[property]['position'][j][0];
+						let units = list[property]['position'][j][1];
+						// convert any type of keys to the pixels
+						list[property]['position'][j] = convert.unitsToPixels(numbers, units) + top;
+					}
+					paramsVerified.push(list);
+				}
+				// make a global list of animated elements with them positions, values and statuses
+				animation.list.push({
+					'element': animation.elements[i],
+					'params': paramsVerified
+				});
+			}
+		},
+		detect: function() {
+			let scroll = window.pageYOffset;
+			for (let i = 0; i < animation.list.length; i++) {
+				let properties = animation.list[i]['params'];
+				for (let j = 0; j < properties.length; j++) {
+					let percent;
+					let property;
+					let params;
+					for (let key in properties[j]) {
+						property = key;
+					}
+					params = properties[j][property];
+					let position = params['position'];
+					let first = 0;
+					let last = position.length-1;
+					// the element is before the first position key of its animation, apply changes only once
+					if (scroll < position[first] && params['status'] != animation.status.before) {
+						params['status'] = animation.status.before;
+						percent = 0;
+						animation.set(animation.list[i], percent, first, first, j, property);
+					}
+					// the element is after the last position key of its animation, apply changes only once
+					else if (scroll >= position[last] && params['status'] != animation.status.after) {
+						params['status'] = animation.status.after;
+						percent = 100;
+						animation.set(animation.list[i], percent, last, last, j, property);
+					}
+					// the element is between the first and last position keys of its animation
+					else if (scroll >= position[first] && scroll < position[last]) {
+						if (params['status'] != animation.status.active) {
+							params['status'] = animation.status.active;
+						}
+						// animation has only two position keys (without intermediate)
+						if (position.length == 2) {
+							percent = convert.pixelsToPercent(position[first], position[last], scroll);
+							animation.set(animation.list[i], percent, first, last, j, property);
+						}
+						// animation has intermediate position keys
+						else {
+							for (let k = 0; k < position.length; k++) {
+								// looking for keys between which we are now
+								if (position[k+1] && scroll >= position[k] && scroll < position[k+1]) {
+									// at the first entry in any case, apply the changes
+									// next time animate only if the values between keys are not identical
+									if (!params['locked']) {
+										percent = convert.pixelsToPercent(position[k], position[k+1], scroll);
+										animation.set(animation.list[i], percent, k, k+1, j, property);
+									}
+									// compare values between keys, if they are the same - lock
+									let start = '';
+									let end = '';
+									for (let l = 0; l < params['values'][k].length; l++) {
+										start += params['values'][k][l];
+										end += params['values'][k+1][l];
+									}
+									if (start == end && !params['locked']) {
+										params['locked'] = true;
+									} else if (start != end && params['locked']) {
+										params['locked'] = false;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		},
+		set: function(element, percent, start, end, id, property) {
+			start = element['params'][id][property]['values'][start];
+			end = element['params'][id][property]['values'][end];
+			// collect the value back to string
+			let value = '';
+			for (let i = 0; i < start.length; i++) {
+				if (percent == 0) {
+					value += start[i];
+				} else if (percent == 100) {
+					value += end[i];
+				} else {
+					if (validation.numbers(start[i])) {
+						value += convert.percentToValue(start[i], end[i], percent);
+					} else {
+						value += start[i];
+					}
+				}
+			}
+			// apply changes to an element
+			window.requestAnimationFrame(function() {
+				if (property != 'opacity') {
+					element['element'].style['-webkit-' + property] = value;
+				}
+				element['element'].style[property] = value;
+			});
+		}
+	};
+
+	let validation = {
+		numbers: function(data) {
+			if (typeof data == 'number') return true;
+			else if (data.match(/\d/)) return true;
+			else return false;
+		},
+		autocomplete: function(data) {
+			// get the longest string
+			let completed = data[0].slice();
+			for (let i = 0; i < data.length; i++) {
+				if (data[i].length > completed.length) {
+					completed = data[i].slice();
+				}
+			}
+			// get the longest substrings except numbers
+			for (let i = 0; i < data.length; i++) {
+				for (let j = 0; j < completed.length; j++) {
+					if (data[i][j] && !validation.numbers(data[i][j])) {
+						if (data[i][j].length > completed[j].length) {
+							completed[j] = data[i][j];
+						}
+					}
+				}
+			}
+			// correct the wrong substrings except numbers, in each string
+			for (let i = 0; i < completed.length; i++) {
+				for (let j = 0; j < data.length; j++) {
+					if (data[j][i]) {
+						if (validation.numbers(data[j][i])) {
+							data[j][i] = +data[j][i];
+						} else if (!validation.numbers(data[j][i]) && data[j][i] != completed[i]) {
+							data[j][i] = completed[i];
+						}
+					} else {
+						if (validation.numbers(completed[i])) {
+							data[j][i] = +completed[i];
+						} else {
+							data[j][i] = completed[i];
+						}
+					}
+				}
+			}
+			return data;
+		}
+	};
+
+	let convert = {
+		unitsToPixels: function(numbers, units) {
+			units = units || 'px';
+			if (units.match('px')) {return numbers}
+			else if (units.match('vh')) {return (numbers / 100) * document.documentElement.clientHeight}
+			else if (units.match('vw')) {return (numbers / 100) * document.documentElement.clientWidth}
+		},
+		pixelsToPercent: function(a, b, pixels) {
+			return ((pixels - a) * 100) / (b - a);
+		},
+		percentToValue: function(a, b, percent) {
+			return ((percent * (b - a)) / 100) + a;
+		}
+	};
+
+	let correctTop = function(element) {
+		let top = 0;
+		while(element && !isNaN(element.offsetTop)) {
+			if (!element.style.position.match('sticky')) {
+				top += element.offsetTop - element.scrollTop;
+			} else {
+				let originalPosition = element.style.position;
+				element.style.position = 'relative';
+				top += element.offsetTop - element.scrollTop;
+				element.style.position = originalPosition;
+			}
+			element = element.offsetParent;
+		}
+		return top;
+	};
+
+	let resizeEnd = function(params) {
+		let startWidth = document.documentElement.clientWidth;
+		let startHeight = document.documentElement.clientHeight;
+		setTimeout(function() {
+			let endWidth = document.documentElement.clientWidth;
+			let endHeight = document.documentElement.clientHeight;
+			if (startWidth == endWidth && startHeight == endHeight) {
+				// end of resize event
+				params();
+			}
+		}, 50);
+	};
+
+
+
+	// initialization
+	let initialization = function () {
+		sticky.wrap();
+		sticky.get();
+		animation.get();
+		animation.detect();
+	};
+	// get data and set keys and params
+	document.addEventListener('DOMContentLoaded', initialization);
+	window.addEventListener('load', initialization);
+	window.addEventListener('resize', function() {
+		resizeEnd(initialization);
+	});
+	// detect animation keys
+	window.addEventListener('scroll', animation.detect);
+
+
+
+}());
